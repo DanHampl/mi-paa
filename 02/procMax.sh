4d6daacab8a0905e1746a3ae23fe083d69e70f37
Daniel Hampl <danhampl31415@gmail.com>
2020-10-25 01:12:55 +0200

    2. Exaktní a heuristická řešení konstruktivního problému batohu

--- 02/procMax.sh
+++ 02/procMax.sh
@@ -10,12 +10,9 @@ do
     for i in 4 10 15 20 22 25 27 30 32 35 37 40
     do
 
-        kek=$(iconv -f utf-16 -t utf-8 data/raw/N"$i"_5.txt | grep hungryExtendedDeviation | tr -s ' '| sed 's/^ *//g' | cut -d' ' -f 3| awk 'BEGIN{a=   0.0}{if ($1>a) a=$1} END{print a/NR}');
-        kek=$(echo "scale=6; $kek"| bc)
-        echo "$i $kek $name$j" >> data/$name$j.txt
-
-        # p=$(echo "scale=6; $var/$kek" | bc)
-        # echo "$i $p fptas$j" >> data/fptasP_$j.txt
+        kek=$(iconv -f utf-16 -t utf-8 data/raw_101/N"$i"_5.txt | grep hungryExtendedDeviation | tr -s ' '| sed 's/^ *//g' | cut -d' ' -f 3);#| awk 'BEGIN{a=0.0}{if ($1>$a) a=$1} END{print a}');
+        #kek=$(echo "scale=6; $kek/1"| bc)
+        echo "$kek" >> data/$name$j-$i.txt
 
     done
 done
